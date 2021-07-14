b51699f4851273df1f3afad6652581d67c889649
Titania01 <adegbitebidmus@gmail.com>
2021-07-14 13:54:34 +0100

    feat: helper function and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,3 +6,10 @@ export const totalLikes = (blogs) => {
   const reduced = (acc, blog) => acc + blog.likes;
   return blogs.reduce(reduced, 0);
 };
+
+export const favoriteBlog = (blogs) => {
+  const favourite = Math.max(...blogs.map((blog) => blog.likes));
+  return blogs
+    .filter((blog) => blog.likes === favourite)
+    .map(({ title, author, likes }) => ({ title, author, likes }))[0];
+};
