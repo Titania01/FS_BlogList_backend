60b4340e0a7b01457e0a8b24b35fd4f773fdc424
Titania01 <adegbitebidmus@gmail.com>
2021-08-26 16:09:30 +0100

    feat: blog list test steps3&4

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -30,9 +30,42 @@ describe("get all blogs", () => {
     expect(response.body).toHaveLength(listHelper.initialBlogs.length);
   }, 100000);
 
-  test("of having unique identifier", () => {
+  test("of having unique identifier", async () => {
     const response = await api.get("/api/blogs");
     expect(response.body[0].id).toBeDefined();
+  });
+
+  test("of blog total number is increased by one", async () => {
+    const newBlog = {
+      title: "Rancho Shalmadas Chanchad",
+      author: "Bidmus Yu",
+      url: "https://reactnative.com/",
+      likes: 7,
+    };
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/blogs");
+    const blogTitle = response.body.map((blog) => blog.title);
+
+    expect(response.body).toHaveLength(listHelper.initialBlogs.length + 1);
+    expect(blogTitle).toContain(newBlog.title);
+  });
+
+  test("of if likes property is missing, should default to zero", async () => {
+    const userData = { username: "Bidmus", password: "sulema" };
+    await api.post("/api/login").send(userData).expect(200);
+
+    const nonLikesBlog = {
+      title: "No one likes Him",
+      author: "Bode Deway",
+      url: "https://hello.com/",
+    };
+    if (!nonLikesBlog.likes) nonLikesBlog.likes = 0;
+    await api.post("api/blogs").send(nonLikesBlog).expect(201);
   });
 });
 
